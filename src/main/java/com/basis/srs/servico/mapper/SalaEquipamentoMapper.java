d41bb5a010431529ef234ef41c624195de410678
matheushenriquelima <mateus.matheus.lima@gmail.com>
2020-10-05 16:15:59 -0300

    Correção SalaEquipamentoMapper e SalaDTO

--- src/main/java/com/basis/srs/servico/mapper/SalaEquipamentoMapper.java
+++ src/main/java/com/basis/srs/servico/mapper/SalaEquipamentoMapper.java
@@ -4,12 +4,9 @@ import com.basis.srs.dominio.SalaEquipamento;
 
 import com.basis.srs.servico.dto.SalaEquipamentoDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {})
 public interface SalaEquipamentoMapper extends EntityMapper<SalaEquipamentoDTO, SalaEquipamento>
 {
 
-    @Mapping(source="salaEquipamentoPK.idSala",target="idSala")
-    SalaEquipamentoDTO toEntity(SalaEquipamento salaEquipamento);
 }
