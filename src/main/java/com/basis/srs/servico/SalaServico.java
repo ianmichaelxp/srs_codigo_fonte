28a63c3a996793830bf523f86cf73e9df4b1cb6b
matheushenriquelima <mateus.matheus.lima@gmail.com>
2020-10-06 13:49:09 -0300

    atualizando SalaServico consertando o postMapping

--- src/main/java/com/basis/srs/servico/SalaServico.java
+++ src/main/java/com/basis/srs/servico/SalaServico.java
@@ -1,6 +1,8 @@
 package com.basis.srs.servico;
 
 import com.basis.srs.dominio.Sala;
+import com.basis.srs.dominio.SalaEquipamento;
+import com.basis.srs.repositorio.SalaEquipamentoRepositorio;
 import com.basis.srs.repositorio.SalaRepositorio;
 import com.basis.srs.servico.dto.SalaDTO;
 import com.basis.srs.servico.mapper.SalaMapper;
@@ -10,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,6 +20,9 @@ import java.util.List;
 @Transactional
 public class SalaServico {
     private final SalaRepositorio salaRepositorio;
+
+    private final SalaEquipamentoRepositorio salaEquipamentoRepositorio;
+
     private final SalaMapper salaMapper;
 
     public List<SalaDTO> listarSalas() {
@@ -29,9 +35,17 @@ public class SalaServico {
         return salaMapper.toDto(salaRepositorio.findById(id).orElse(null));
     }
 
-    public Sala salvarSala(@RequestBody SalaDTO salaDTO) {
-
-        return salaRepositorio.save(salaMapper.toEntity(salaDTO));
+    public SalaDTO salvarSala(@RequestBody SalaDTO salaDTO) {
+        Sala sala = salaMapper.toEntity(salaDTO);
+        List<SalaEquipamento> equipamentos = sala.getEquipamentos();
+        sala.setEquipamentos(new ArrayList<>());
+        salaRepositorio.save(sala);
+        equipamentos.forEach(equipamento -> {
+           equipamento.setSala(sala);
+           equipamento.getSalaEquipamentoPK().setIdSala(sala.getId());
+        });
+        salaEquipamentoRepositorio.saveAll(equipamentos);
+        return salaMapper.toDto(sala);
     }
 
     public void removerSala(@PathVariable(value = "id") Integer id) {
--- src/main/java/com/basis/srs/web/rest/SalaRecursos.java
+++ src/main/java/com/basis/srs/web/rest/SalaRecursos.java
@@ -1,6 +1,5 @@
 package com.basis.srs.web.rest;
 
-import com.basis.srs.dominio.Sala;
 import com.basis.srs.servico.dto.SalaDTO;
 import com.basis.srs.servico.SalaServico;
 import lombok.RequiredArgsConstructor;
@@ -14,32 +13,34 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/salas")
 @RequiredArgsConstructor
-public class SalaRecursos
-{
+public class SalaRecursos {
     private final SalaServico salaServico;
 
     @GetMapping
-    public ResponseEntity<List<SalaDTO>> listarSalas(){
+    public ResponseEntity<List<SalaDTO>> listarSalas() {
 
         return ResponseEntity.ok(salaServico.listarSalas());
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<SalaDTO> listarSala(@PathVariable(value = "id") Integer id){
+    public ResponseEntity<SalaDTO> listarSala(@PathVariable(value = "id") Integer id) {
 
         return ResponseEntity.ok(salaServico.listarSala(id));
     }
+
     @PostMapping
-    public ResponseEntity<Sala> criarSala(@RequestBody SalaDTO dto) throws URISyntaxException
-    {
+    public ResponseEntity<SalaDTO> criarSala(@RequestBody SalaDTO dto) throws URISyntaxException {
         return ResponseEntity.created(new URI("/api/clientes/")).body(salaServico.salvarSala(dto));
     }
+
     @PutMapping
-    public ResponseEntity<Sala> atualizarSala(@RequestBody SalaDTO salaDTO){
+    public ResponseEntity<SalaDTO> atualizarSala(@RequestBody SalaDTO salaDTO) {
         SalaDTO dto = new SalaDTO();
         return ResponseEntity.ok(salaServico.salvarSala(dto));
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> removerSala(@PathVariable(value = "id")Integer id){
+    public ResponseEntity<Void> removerSala(@PathVariable(value = "id") Integer id) {
         salaServico.removerSala(id);
         return ResponseEntity.ok().build();
     }
